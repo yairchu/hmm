39fcedd186d10d9a9df3ff6b8dc8775dcfdcbaea
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2010-02-08 17:26:27 +0200

    towards viterbi

--- src/Numeric/HMM/Sparse.hs
+++ src/Numeric/HMM/Sparse.hs
@@ -1,30 +1,42 @@
 {-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
 
 module Numeric.HMM.Sparse
-  ( Hmm(..), HmmCalc(..)
-  , hmmCLayerProbs, hmmCalc, hmmGenerate
+  ( Hmm(..), HmmFwdBwd(..), HmmViterbi(..)
   , makeDenseHmm
+  , hmmGenerate
+  , hmmLayerProbs
+  , hmmViterbi, hmmViterbiCertainty
   ) where
 
 import Numeric.Probability.Discrete
 
-import Control.Applicative (liftA2)
-import Control.Monad (liftM)
+import Control.Applicative ((<$>), (<*>), ZipList(..), liftA2)
+import Control.Monad (forM_, liftM)
 import Control.Monad.ListT (ListT)
 import Control.Monad.Random.Class (MonadRandom)
+import Control.Monad.ST
 import Data.Array.IArray (IArray, Array, (!), listArray)
+import Data.Array.MArray
+import Data.Array.ST (STUArray)
 import Data.Array.Unboxed (UArray)
 import Data.Ix (Ix, range, rangeSize)
 import Data.List.Class (iterateM, toList)
+import Data.STRef
 import System.Random (Random)
+
+data HmmLayerDesc state =
+  HmmLayerDesc
+  { hmmLayerSize :: Int
+  , hmmLayerStates :: Int -> state
+  , hmmLayerTransitionsFromPrev :: state -> [Int]
+  }
 
 data Hmm state obs prob =
   Hmm
   { hmmStartProbs :: Probs state prob
   , hmmTransitionProbs :: state -> Probs state prob
   , hmmObservationProbs :: state -> Probs obs prob
-  , hmmStatesForObservation :: obs -> (Int, Int -> state)
-  , hmmMaxStatesForObservation :: Maybe Int
+  , hmmStatesForObservation :: obs -> HmmLayerDesc state
   }
 
 funcArray :: (IArray a e, Ix i) => (i, i) -> (i -> e) -> a i e
@@ -34,17 +46,22 @@ funcArray rng func = listArray rng . map func $ range rng
 -- Uses O(N) memory where N is number of variables in model,
 -- (for fast output generation).
 makeDenseHmm
-  :: forall s o p. (Floating p, Ix s, IArray UArray p)
-  => (s, s) -> [o]
+  :: forall s o p. (Floating p, Ix s, Integral s, Integral o, IArray UArray p)
+  => (s, s) -> (o, o)
   -> (s -> p) -> (s -> s -> p) -> (s -> o -> p)
   -> Hmm s o p
-makeDenseHmm statesRange obs startProbs transProbs obsProbs =
+makeDenseHmm statesRange obsRange startProbs transProbs obsProbs =
   Hmm
-  { hmmStartProbs = makeProbsFast states startProbs
+  { hmmStartProbs = makeProbsFast statesRange startProbs
   , hmmTransitionProbs = (transArr !)
   , hmmObservationProbs = (obsArr !)
-  , hmmStatesForObservation = const (numStates, (statesArr !))
-  , hmmMaxStatesForObservation = Just numStates
+  , hmmStatesForObservation =
+      const
+      HmmLayerDesc
+      { hmmLayerSize = numStates
+      , hmmLayerStates = (statesArr !)
+      , hmmLayerTransitionsFromPrev = const [0 .. numStates - 1]
+      }
   }
   where
     numStates = rangeSize statesRange
@@ -52,9 +69,9 @@ makeDenseHmm statesRange obs startProbs transProbs obsProbs =
     statesArr :: Array Int s
     statesArr = listArray (0, numStates - 1) states
     transArr :: Array s (Probs s p)
-    transArr = funcArray statesRange (makeProbsFast states . transProbs)
+    transArr = funcArray statesRange (makeProbsFast statesRange . transProbs)
     obsArr :: Array s (Probs o p)
-    obsArr = funcArray statesRange (makeProbsFast obs . obsProbs)
+    obsArr = funcArray statesRange (makeProbsFast obsRange . obsProbs)
 
 hmmGenerate
   :: (MonadRandom m, Fractional prob, Ord prob, Random prob)
@@ -73,19 +90,94 @@ hmmGenerate hmm
       = probsRandom (hmmTransitionProbs hmm state)
       >>= addObs
 
-data HmmCalc state prob =
-  HmmCalc
-  { hmmCForward :: Int -> state -> prob
-  , hmmCBackward :: Int -> state -> prob
-  , hmmCLayerMult :: Int -> prob
-  , hmmCViterbi :: [state]
+data HmmFwdBwd state prob =
+  HmmFwdBwd
+  { -- | Forward algorithm. Including current layer's weight.
+    -- Each layer is normalized.
+    hmmForward :: Int -> state -> prob
+  , -- | Backward algorithm. Not including current layer's weights
+    -- Each layer is normalized.
+    hmmBackward :: Int -> state -> prob
+  , -- | Probabilty for observation according to model.
+    hmmLogProb :: prob
+  , hmmLayerMult :: Int -> prob
   }
 
-hmmCLayerProbs :: Num prob => HmmCalc state prob -> Int -> state -> prob
-hmmCLayerProbs
-  = (liftA2 . liftA2) (fmap . (*)) hmmCLayerMult
-  $ (liftA2 . liftA2 . liftA2) (*) hmmCForward hmmCBackward
+data HmmViterbi state prob =
+  HmmViterbi
+  { hmmViterbiPath :: [state]
+  , hmmViterbiLogProb :: prob
+  }
 
-hmmCalc :: Hmm state obs prob -> [obs] -> HmmCalc state prob
-hmmCalc = undefined
+hmmLayerProbs :: Num prob
+  => HmmFwdBwd state prob -> Int -> state -> prob
+hmmLayerProbs
+  = (liftA2 . liftA2) (fmap . (*)) hmmLayerMult
+  $ (liftA2 . liftA2 . liftA2) (*) hmmForward hmmBackward
+
+hmmViterbiCertainty :: Num prob
+  => HmmViterbi state prob -> HmmFwdBwd state prob -> prob
+hmmViterbiCertainty v fb = hmmViterbiLogProb v - hmmLogProb fb
+
+class Unboxed a where
+  newSTUArray :: Ix i => (i, i) -> a -> ST s (STUArray s i a)
+  readSTUArray :: Ix i => STUArray s i a -> i -> ST s a
+  writeSTUArray :: Ix i => STUArray s i a -> i -> a -> ST s ()
+
+instance Unboxed Float where
+  newSTUArray = newArray
+  readSTUArray = readArray
+  writeSTUArray = writeArray
+
+instance Unboxed Double where
+  newSTUArray = newArray
+  readSTUArray = readArray
+  writeSTUArray = writeArray
+
+modifySTUArray :: (Unboxed a, Ix i) => STUArray s i a -> i -> (a -> a) -> ST s ()
+modifySTUArray arr idx func =
+  readSTUArray arr idx >>= writeSTUArray arr idx . func
+
+hmmViterbi
+  :: forall state obs prob
+   . (Unboxed prob, Floating prob, Ord prob)
+  => Hmm state obs prob -> [obs] -> HmmViterbi state prob
+hmmViterbi model observations = runST $ do
+  arr <- newSTUArray (0, arrSize - 1) 0 :: ST s (STUArray s Int prob)
+  let
+    addLayerWeights arrIdx layer probFunc =
+      forM_ [0 .. hmmLayerSize layer - 1] $ \i ->
+        modifySTUArray arr (arrIdx + i) (+ probFunc (hmmLayerStates layer i))
+    addLayerObsWeights arrIdx layer obser =
+      addLayerWeights arrIdx layer
+        ((`probsLogs` obser) . hmmObservationProbs model)
+  arrIdxRef <- newSTRef 0
+  forM_
+    (getZipList ((,,)
+    <$> ZipList revLayers
+    <*> ZipList (tail revLayers)
+    <*> ZipList (reverse observations)
+    )) $ \(layerN, layerP, obsN) -> do
+      arrIdxN <- readSTRef arrIdxRef
+      let arrIdxP = arrIdxN + hmmLayerSize layerN
+      writeSTRef arrIdxRef arrIdxP
+      addLayerObsWeights arrIdxN layerN obsN
+      forM_ [0 .. hmmLayerSize layerP] $ \iP -> do
+        let
+          stateP = hmmLayerStates layerP iP
+          (iN0 : iNs) = hmmLayerTransitionsFromPrev layerN stateP
+        readSTUArray arr (arrIdxN + iN0)
+          >>= writeSTUArray arr (arrIdxP + iP)
+        forM_ iNs $ \iN ->
+          readSTUArray arr (arrIdxN + iN)
+            >>= modifySTUArray arr (arrIdxP + iP) . max
+  layer0Idx <- readSTRef arrIdxRef
+  addLayerObsWeights layer0Idx (head layers) (head observations)
+  addLayerWeights layer0Idx (head layers) . probsLogs $ hmmStartProbs model
+  undefined
+  where
+    arrSize = sum $ map hmmLayerSize layers
+    layers = map (hmmStatesForObservation model) observations
+    revLayers = reverse layers
+    -- nodeWeights = probsLogs $ 
 
--- src/Numeric/Probability/Discrete.hs
+++ src/Numeric/Probability/Discrete.hs
@@ -2,13 +2,13 @@
 
 module Numeric.Probability.Discrete
   ( Probs(..)
-  , probsIndex, probsRandom, probsLogs
+  , probsIndex, probsRandom, probsLogs, probsStates
   , makeProbsFast, makeProbsLean
   ) where
 
 import Control.Monad (liftM)
 import Control.Monad.Random.Class (MonadRandom(getRandom))
-import Data.Array.IArray (IArray, Array, (!), listArray)
+import Data.Array.IArray (IArray, (!), listArray)
 import Data.Array.Unboxed (UArray)
 import Data.Maybe (fromJust)
 import Numeric.Search.Range (searchFromTo)
@@ -23,7 +23,8 @@ data Probs state prob =
 data ProbsExt state prob
   = ProbsLean [state]
   | ProbsFast
-    { probsState :: Int -> state
+    { probsStateIdx :: state -> Int
+    , probsState :: Int -> state
     , probsLog :: Int -> prob
     , probsAccum :: Int -> prob
     , probsNumStates :: Int
@@ -36,21 +37,22 @@ makeProbsLean states func =
   Probs func $ ProbsLean states
 
 makeProbsFast
-  :: forall prob state. (Floating prob, IArray UArray prob)
-  => [state] -> (state -> prob) -> Probs state prob
-makeProbsFast states func =
+  :: forall prob state
+   . (Integral state, Floating prob, IArray UArray prob)
+  => (state, state) -> (state -> prob) -> Probs state prob
+makeProbsFast (stateStart, stateEnd) func =
   Probs func $
   ProbsFast
   { probsNumStates = numStates
+  , probsState = (+ stateStart) . fromIntegral
+  , probsStateIdx = fromIntegral . (+ negate stateStart)
   , probsLog = (logsArr !)
-  , probsState = (stateArr !)
   , probsAccum = (accumArr !)
   }
   where
-    numStates = length states
+    numStates = fromIntegral $ stateEnd + 1 - stateStart
+    states = [stateStart .. stateEnd]
     rng = (0, numStates - 1)
-    stateArr :: Array Int state
-    stateArr = listArray rng states
     logsArr :: UArray Int prob
     logsArr = listArray rng . map (log . func) $ states
     accumArr :: UArray Int prob
@@ -58,6 +60,10 @@ makeProbsFast states func =
       = listArray rng
       . scanl (+) 0 . map func
       . init $ states
+
+probsStates :: Probs state prob -> [state]
+probsStates (Probs _ (ProbsLean states)) = states
+probsStates (Probs _ pf) = map (probsState pf) [0 .. probsNumStates pf - 1]
 
 probsIndex :: Ord prob => Probs state prob -> prob -> state
 probsIndex (Probs probFunc (ProbsLean states)) idx
@@ -70,16 +76,9 @@ probsIndex (Probs _ pf) idx
   = probsState pf . fromJust
   $ searchFromTo ((>= idx) . probsAccum pf) 0 (probsNumStates pf - 1)
 
-
-probsLogs :: Floating prob => Probs state prob -> [(state, prob)]
-probsLogs (Probs probFunc (ProbsLean states)) =
-  map f states
-  where
-    f x = (x, (log . probFunc) x)
-probsLogs (Probs _ pf) =
-  map f [0 .. probsNumStates pf - 1]
-  where
-    f i = (probsState pf i, probsLog pf i)
+probsLogs :: Floating prob => Probs state prob -> state -> prob
+probsLogs (Probs probFunc (ProbsLean _)) = log . probFunc
+probsLogs (Probs _ pf) = probsLog pf . probsStateIdx pf
 
 probsRandom
   :: (Fractional prob, Ord prob, Random prob, MonadRandom m)
