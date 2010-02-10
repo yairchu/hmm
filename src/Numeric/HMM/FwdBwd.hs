88ce13f26ca7d625093c8bdbe54176140f3b86c3
Yair Chuchem <yairchu@yair-chuchems-macbook.local>
2010-02-10 23:35:38 +0200

    need refactoring

--- src/Numeric/HMM/FwdBwd.hs
+++ src/Numeric/HMM/FwdBwd.hs
@@ -2,9 +2,11 @@
 
 module Numeric.HMM.FwdBwd
   ( HmmFwdBwd(..)
+  , hmmFwdBwd
   , hmmLayerProbs
   ) where
 
+import Data.Array.Unboxed.YC
 import Numeric.HMM
 import Numeric.HMM.Internal
 
@@ -12,22 +14,30 @@ import Control.Applicative (liftA2)
 
 data HmmFwdBwd state prob =
   HmmFwdBwd
-  { hmmFwdBwdNumLayers :: Int
+  { hmmLayers :: Int -> HmmLayerDesc state
   , -- | Forward algorithm. Including current layer's weight.
     -- Each layer is normalized.
-    hmmForward :: Int -> state -> prob
+    hmmForward :: Int -> Int -> prob
   , -- | Backward algorithm. Not including current layer's weights
     -- Each layer is normalized.
-    hmmBackward :: Int -> state -> prob
+    hmmBackward :: Int -> Int -> prob
   , -- | Probabilty for observation according to model.
     hmmLogProb :: prob
   , hmmLayerMult :: Int -> prob
-  , hmmLayers :: Int -> HmmLayerDesc state
   }
 
 hmmLayerProbs :: Num prob
-  => HmmFwdBwd state prob -> Int -> state -> prob
+  => HmmFwdBwd state prob -> Int -> Int -> prob
 hmmLayerProbs
   = (liftA2 . liftA2) (fmap . (*)) hmmLayerMult
   $ (liftA2 . liftA2 . liftA2) (*) hmmForward hmmBackward
+
+hmmFwdBwd
+  :: (Unboxed prob, Num prob)
+  => Hmm state obs prob -> Int -> (Int -> obs) -> HmmFwdBwd state prob
+hmmFwdBwd model numObs getObs =
+  HmmFwdBwd
+  { hmmLayers = hmmStatesForObservation model . getObs
+  , hmmBackward = backwardAlgorithmH l1Mode model $ map getObs [0 .. numObs - 1]
+  }
 
